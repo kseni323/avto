071add7372c6f7b4b670446325a4588704c33094
kseni <ksenialukianova2000@gmail.com>
2024-11-05 20:57:23 +0300

    first commit

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    use HasFactory;
+
+    // Добавляем `pickup_location` в массив $fillable
+    protected $fillable = [
+        'pickup_location',
+        'return_location',
+        'pickup_date',
+        'return_date',
+        'car_type',
+        // Другие поля, если они есть
+    ];
 }
