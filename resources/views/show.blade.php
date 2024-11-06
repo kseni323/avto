be9619fa2f496e16cb5a8cb2bf76d01aa4d65c1b
kseni <ksenialukianova2000@gmail.com>
2024-11-07 00:11:27 +0300

    first commit

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,14 +1,6 @@
 @extends('header')
 
 @section('show_content')
-
-<head>
-    <meta charset="UTF-8">
-    <title>Автомобили</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
 <body>
     <div class="container my-5">
         <div class="row">
