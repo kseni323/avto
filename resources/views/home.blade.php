afbbcc16ee14627a9f7ada9a862fc3dec31a4d09
kseni <ksenialukianova2000@gmail.com>
2024-11-07 02:07:53 +0300

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/app1.css">
     <link rel="stylesheet" href="css/app3.css">
-    <link rel="stylesheet" href="css/app.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="js/forms.js" defer></script>
