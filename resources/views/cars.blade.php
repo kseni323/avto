955b986b5a3ea251c50669400772cb772b2b98b8
kseni <ksenialukianova2000@gmail.com>
2024-11-07 19:48:19 +0300

    first commit

--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -5,13 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Автомобили - driveGo</title>
+    <link rel="stylesheet" href="css/app1.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="{{ asset('js/carFilter.js') }}"></script>
 </head>
-<style>
-</style>
 <body>
     <!-- Фильтры для поиска автомобилей -->
     <section class="container my-4">
