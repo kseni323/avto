b1420fb6f2ef2d7a3d7b6f4b9e9bba2b0e3c39d0
kseni <ksenialukianova2000@gmail.com>
2024-11-07 23:21:46 +0300

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -51,15 +51,7 @@
         font-size: 0.9em;
         bottom: 10px; /* Снижение текста вниз */
     }
-    .btn {
-    display: inline-block;
-    padding: 10px 20px;
-    background: #04DBC0;
-    color: white;
-    text-decoration: none;
-    border-radius: 5px;
-    font-size: 1.1em;
-}
+    
 }
 
 </style>
