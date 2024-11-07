1013692ab143cd2789073f2eed6ba39dddb31dbc
kseni <ksenialukianova2000@gmail.com>
2024-11-07 22:05:22 +0300

    first commit

--- app/Http/Controllers/CarController.php
+++ app/Http/Controllers/CarController.php
@@ -5,34 +5,25 @@ namespace App\Http\Controllers;
 use App\Models\Car;
 class CarController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        // Получаем параметры фильтров из запроса
-        $class = $request->input('class');
-        $transmission = $request->input('transmission');
-        $drive_type = $request->input('drive_type');
-    
-        // Строим запрос с учетом фильтров
-        $query = Car::query();
-    
-        if ($class) {
-            $query->where('class', $class);
-        }
-    
-        if ($transmission) {
-            $query->where('transmission', $transmission);
-        }
-    
-        if ($drive_type) {
-            $query->where('drive_type', $drive_type);
-        }
-    
-        // Получаем результаты фильтрации
-        $cars = $query->get();
-    
-        // Возвращаем вид с отфильтрованными машинами
-        return view('cars.index', compact('cars'));
+        // Получить все автомобили для начального отображения
+        $cars = Car::all();
+        return view('cars', compact('cars'));
     }
+
+public function filter(Request $request)
+{
+    $cars = Car::query()
+        ->when($request->class, fn($query) => $query->where('class', $request->class))
+        ->when($request->transmission, fn($query) => $query->where('transmission', $request->transmission))
+        ->when($request->drive_type, fn($query) => $query->where('drive_type', $request->drive_type))
+        ->get();
+
+    $html = view('partials.car_results', compact('cars'))->render();
+
+    return response()->json(['html' => $html]);
+}
 
 public function show($id)
 {
