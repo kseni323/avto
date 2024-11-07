c3995ae86d86544f9ecfd40ee0a2155bcc93ca7c
kseni <ksenialukianova2000@gmail.com>
2024-11-07 19:56:35 +0300

    first commit

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <title>Автомобили</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
@@ -299,23 +300,6 @@ element.style {
   document.getElementById("subscribeButton").addEventListener("click", subscribeUser);
 </script>
 </section>
-
-<footer class="footer_section">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-6">
-						<div class="copyright">
-							driveGo© 
-							<script type="text/javascript"> 
-								document.write(new Date().getFullYear())
-							</script>
-							Все правы защищены
-						</div>
-					</div>
-				</div>
-			</div>
-		</footer>
-
 
 <script>
     document.addEventListener('DOMContentLoaded', function () {
