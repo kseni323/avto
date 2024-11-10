1008ddd452ea390b966d78ad731f05eb3039dbcb
kseni <ksenialukianova2000@gmail.com>
2024-11-10 23:44:01 +0300

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -25,17 +25,23 @@
     box-sizing: border-box;
 }
 
-    .hero-section {
-        padding: 2px 1px; /* Сокращение внутренних отступов */
-    }
-
-    .content {
+.hero-section {
     position: relative;
-    z-index: 1; 
-    text-align: left;
-    color: #fff; 
-    font-family: "Montserrat";
-    padding-left: 152px;
+}
+
+.hero-section .content {
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.hero-section .content h1,
+.hero-section .content p,
+.hero-section .content .btn {
+    margin: 5px 0;
 }
 
     .footer_widget .contact_info li i {
@@ -76,7 +82,7 @@
     <div class="container marketing"> 
     <div class="section-title"> Какие услуги мы предлагаем нашим клиентам</div>
     <div class="row text-center">
-        <div class="col-lg-4">
+        <div class="col-lg-4 section-padding">
         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-car-front" viewBox="0 0 16 16">
   <path d="M4 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0m10 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0M6 8a1 1 0 0 0 0 2h4a1 1 0 1 0 0-2zM4.862 4.276 3.906 6.19a.51.51 0 0 0 .497.731c.91-.073 2.35-.17 3.597-.17s2.688.097 3.597.17a.51.51 0 0 0 .497-.731l-.956-1.913A.5.5 0 0 0 10.691 4H5.309a.5.5 0 0 0-.447.276"/>
   <path d="M2.52 3.515A2.5 2.5 0 0 1 4.82 2h6.362c1 0 1.904.596 2.298 1.515l.792 1.848c.075.175.21.319.38.404.5.25.855.715.965 1.262l.335 1.679q.05.242.049.49v.413c0 .814-.39 1.543-1 1.997V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.338c-1.292.048-2.745.088-4 .088s-2.708-.04-4-.088V13.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-1.892c-.61-.454-1-1.183-1-1.997v-.413a2.5 2.5 0 0 1 .049-.49l.335-1.68c.11-.546.465-1.012.964-1.261a.8.8 0 0 0 .381-.404l.792-1.848ZM4.82 3a1.5 1.5 0 0 0-1.379.91l-.792 1.847a1.8 1.8 0 0 1-.853.904.8.8 0 0 0-.43.564L1.03 8.904a1.5 1.5 0 0 0-.03.294v.413c0 .796.62 1.448 1.408 1.484 1.555.07 3.786.155 5.592.155s4.037-.084 5.592-.155A1.48 1.48 0 0 0 15 9.611v-.413q0-.148-.03-.294l-.335-1.68a.8.8 0 0 0-.43-.563 1.8 1.8 0 0 1-.853-.904l-.792-1.848A1.5 1.5 0 0 0 11.18 3z"/>
@@ -84,7 +90,7 @@
             <h2 class="fw-normal">Краткосрочная аренда</h2>
             <p>Идеально подходит для тех, кто нуждается в автомобиле на несколько дней или недель</p>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 section-padding">
         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-bus-front" viewBox="0 0 16 16">
   <path d="M5 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0m8 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0m-6-1a1 1 0 1 0 0 2h2a1 1 0 1 0 0-2zm1-6c-1.876 0-3.426.109-4.552.226A.5.5 0 0 0 3 4.723v3.554a.5.5 0 0 0 .448.497C4.574 8.891 6.124 9 8 9s3.426-.109 4.552-.226A.5.5 0 0 0 13 8.277V4.723a.5.5 0 0 0-.448-.497A44 44 0 0 0 8 4m0-1c-1.837 0-3.353.107-4.448.22a.5.5 0 1 1-.104-.994A44 44 0 0 1 8 2c1.876 0 3.426.109 4.552.226a.5.5 0 1 1-.104.994A43 43 0 0 0 8 3"/>
   <path d="M15 8a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1V2.64c0-1.188-.845-2.232-2.064-2.372A44 44 0 0 0 8 0C5.9 0 4.208.136 3.064.268 1.845.408 1 1.452 1 2.64V4a1 1 0 0 0-1 1v2a1 1 0 0 0 1 1v3.5c0 .818.393 1.544 1 2v2a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5V14h6v1.5a.5.5 0 0 0 .5.5h2a.5.5 0 0 0 .5-.5v-2c.607-.456 1-1.182 1-2zM8 1c2.056 0 3.71.134 4.822.261.676.078 1.178.66 1.178 1.379v8.86a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 11.5V2.64c0-.72.502-1.301 1.178-1.379A43 43 0 0 1 8 1"/>
@@ -92,12 +98,12 @@
             <h2 class="fw-normal">Долгосрочная аренда</h2>
             <p>Отличный вариант для длительных путешествий или временной замены вашего автомобиля.</p>
         </div>
-        <div class="col-lg-4">
+        <div class="col-lg-4 section-padding">
            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-emoji-sunglasses" viewBox="0 0 16 16">
   <path d="M4.968 9.75a.5.5 0 1 0-.866.5A4.5 4.5 0 0 0 8 12.5a4.5 4.5 0 0 0 3.898-2.25.5.5 0 1 0-.866-.5A3.5 3.5 0 0 1 8 11.5a3.5 3.5 0 0 1-3.032-1.75M7 5.116V5a1 1 0 0 0-1-1H3.28a1 1 0 0 0-.97 1.243l.311 1.242A2 2 0 0 0 4.561 8H5a2 2 0 0 0 1.994-1.839A3 3 0 0 1 8 6c.393 0 .74.064 1.006.161A2 2 0 0 0 11 8h.438a2 2 0 0 0 1.94-1.515l.311-1.242A1 1 0 0 0 12.72 4H10a1 1 0 0 0-1 1v.116A4.2 4.2 0 0 0 8 5c-.35 0-.69.04-1 .116"/>
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0m-1 0A7 7 0 1 0 1 8a7 7 0 0 0 14 0"/>
 </svg>
-            <h2 class="fw-normal">Аренда автомобилей класса люкс</h2>
+            <h2 class="fw-normal ">Аренда автомобилей класса люкс</h2>
             <p>Для тех, кто предпочитает передвигаться с комфортом и стилем.</p>
         </div>
     </div>
@@ -129,7 +135,7 @@
         <img src="images/minicooper.jpg" class="d-block w-100 carousel-img" alt="Second slide">
         <div class="container">
           <div class="carousel-caption text-end">
-            <h1>MINI Cooper</h1>
+            <h1>MINI</h1>
             <p><a class="btn btn-lg btn-primary" href="/cars">ПОСМОТРЕТЬ ВСЕ</a></p>
           </div>
         </div>
@@ -253,7 +259,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-4 col-md-6">
-                <div class="footer_widget">
+                <div class="footer_widget section-padding">
                     <a class="navbar-brand" href=""><ya-tr-span data-index="123-0" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="drive" data-translation="drive" data-ch="0" data-type="trSpan" style="visibility: inherit !important;">drive</ya-tr-span><span style="color:#04DBC0"><ya-tr-span data-index="123-1" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="Go" data-translation="Вперед" data-ch="0" data-type="trSpan" style="visibility: inherit !important;">Go</ya-tr-span></span><ya-tr-span data-index="123-2" data-translated="true" data-source-lang="en" data-target-lang="ru" data-value="" data-translation="" data-ch="0" data-type="trSpan" style="visibility: inherit !important;"> </ya-tr-span></a>
                     <p>Путешествие с хорошими друзьями подарит<br>вам незабываемые впечатления.</p>
                 </div>
@@ -261,7 +267,7 @@
             <div class="col-lg-4 col-md-6">
     <div class="footer_widget">
         <h3>Контактная информация</h3>
-        <ul class="contact_info">
+        <ul class="contact_info section-padding">
             <li>
                 <i class="fas fa-map-marker-alt"></i> Воронеж, ул.Колесниченко, 15
             </li>
@@ -276,7 +282,8 @@
 </div>
 
             <div class="col-lg-4 col-md-6">
-  <div class="footer_widget">
+
+  <div class="footer_widget section-padding">
     <h3>Рассылка</h3>
     <p style="margin-bottom:0px">Ничего не пропустите! Подпишитесь, чтобы получать ежедневные предложения</p>
     
