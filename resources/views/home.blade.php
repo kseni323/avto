366460a39e61a5f1f611102311d85f8dfd49ef45
kseni <ksenialukianova2000@gmail.com>
2024-11-11 00:22:27 +0300

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,7 +8,6 @@
     <title>Аренда автомобилей</title>
     <link rel="stylesheet" href="css/app1.css">
     <link rel="stylesheet" href="css/app232.css">
-    <link rel="stylesheet" href="css/app.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
@@ -32,7 +31,7 @@
 .hero-section .content {
     position: absolute;
     bottom: 10px;
-    left: 10px;
+
     display: flex;
     flex-direction: column;
     align-items: flex-start;
