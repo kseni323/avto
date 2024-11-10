d1a55a7fa48ed35b7d2ea447923ffdd12c44dfbd
kseni <ksenialukianova2000@gmail.com>
2024-11-11 01:08:49 +0300

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -95,19 +95,7 @@
   width: 70%;
 }
 
-.btn-primaary {
-    background-color: #04DBC0;
-    font-family: "Work Sans", sans-serif;
-    font-size: 15px;
-    font-weight: 100;
-    line-height: 50px;
-    display: inline-block;
-    padding: 0 10px;
-    width: 70%;
-    cursor: pointer;
-    border: none;
-    transition: all 0.5s ease-in-out;
-}
+
 }
 </style>
 
