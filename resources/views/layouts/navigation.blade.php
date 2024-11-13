c7618645f153c3bba04ea6a64ef674beca5131a9
kseni <ksenialukianova2000@gmail.com>
2024-11-13 18:54:13 +0300

    first commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,6 +34,9 @@
                     </x-slot>
 
                     <x-slot name="content">
+                    <x-dropdown-link :href="route('index')">
+                            {{ __('Главная') }}
+                        </x-dropdown-link>
                         <x-dropdown-link :href="route('profile.edit')">
                             {{ __('Профиль') }}
                         </x-dropdown-link>
