f9d27b188e705b2520b13e5c59dd8ddd20da1113
kseni <ksenialukianova2000@gmail.com>
2024-11-13 18:58:30 +0300

    first commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,7 +34,7 @@
                     </x-slot>
 
                     <x-slot name="content">
-                    <x-dropdown-link :href="route('index')">
+                    <x-dropdown-link :href="route('home')">
                             {{ __('Главная') }}
                         </x-dropdown-link>
                         <x-dropdown-link :href="route('profile.edit')">
@@ -83,6 +83,9 @@
             </div>
 
             <div class="mt-3 space-y-1">
+            <x-dropdown-link :href="route('home')">
+                            {{ __('Главная') }}
+                        </x-dropdown-link>
                 <x-responsive-nav-link :href="route('profile.edit')">
                     {{ __('Профиль') }}
                 </x-responsive-nav-link>
