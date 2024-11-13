c198a842cf6b546b1f564e760905b2ee60ae14f0
kseni <ksenialukianova2000@gmail.com>
2024-11-13 22:52:22 +0300

    first commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,47 +1,3 @@
-@extends('header')
-
-@section('regi_content')
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Аренда автомобилей</title>
-    <link rel="stylesheet" href="css/app1.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</head>
-</body>
-<nav class="navbar navbar-expand-lg navbar-dark bg-lignt">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <span class="drive-text">drive</span><span style="color:#04DBC0">Go</span>&nbsp;
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mobileNavbar" aria-controls="mobileNavbar" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>      
-        <div class="collapse navbar-collapse" id="mobileNavbar">
-            <ul class="navbar-nav ms-auto">
-        <li class="nav-item active">
-            <a class="nav-link" href="{{ url('/') }}">Главная</a>
-       </li>
-       <li class="nav-item">
-           <a class="nav-link" href="{{ url('/#services') }}">Услуги</a>
-     </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('cars.index') }}">Автомобили</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link" href="{{ url('/#reserve') }}">Бронирование</a>
-    </li>
-    <li class="nav-item">
-         <a class="nav-link" href="{{ url('/#contact-us') }}">Контакты</a>
-    </li>
-            </ul>
-        </div>
-    </div>
-</nav>
-
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
@@ -94,5 +50,3 @@
         </div>
     </form>
 </x-guest-layout>
-</body>
-@endsection
