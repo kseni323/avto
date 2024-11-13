9c03aadc8e9c899042278391a1e3365f3d0753f6
kseni <ksenialukianova2000@gmail.com>
2024-11-13 22:48:49 +0300

    first commit

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,8 +2,16 @@
 
 @section('regi_content')
 
-
-
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Аренда автомобилей</title>
+    <link rel="stylesheet" href="css/app1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+</body>
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
@@ -56,5 +64,5 @@
         </div>
     </form>
 </x-guest-layout>
-
+</body>
 @endsection
