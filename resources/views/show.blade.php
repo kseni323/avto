f3fa3546aed91b21da2908115bd5eb4bdbbffa1f
kseni <ksenialukianova2000@gmail.com>
2024-11-13 20:37:17 +0300

    first commit

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -11,6 +11,10 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/app1.css">
+    <link rel="stylesheet" href="css/app232.css">
+    <link rel="stylesheet" href="css/app.css">
+    <script src="js/forms.js" defer></script>
 </head>
 <style>
     /* Обнуляем отступы и размеры */
