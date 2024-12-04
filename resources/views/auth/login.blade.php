751e2b73f29bd9110982d5ca1c17ac7fa8c3efd1
kseni <ksenialukianova2000@gmail.com>
2024-12-04 20:59:39 +0300

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@
                     {{ ('Забыли пароль') }}
                 </a>
             @endif
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+            <a class="block mt-1 w-full" href="{{ route('register') }}">
                 {{ ('Нет аккаунта?') }}
             </a>
             <x-primary-button class="ms-3">
