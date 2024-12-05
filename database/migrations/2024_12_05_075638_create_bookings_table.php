fc861bfc738faa0013a60f12f2f72da1c105c05b
kseni <ksenialukianova2000@gmail.com>
2024-12-05 22:37:00 +0300

    first commit

--- database/migrations/2024_12_05_075638_create_bookings_table.php
+++ database/migrations/2024_12_05_075638_create_bookings_table.php
@@ -26,3 +26,11 @@ return new class extends Migration
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('bookings');
+    }
+};
