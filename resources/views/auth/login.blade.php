cdbc842f881c5b7060fe8ae8bf7c2dee05a2c70e
kseni <ksenialukianova2000@gmail.com>
2024-12-05 06:51:56 +0300

    first commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -40,10 +40,6 @@
         <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mx-4" href="{{ route('password.request') }}">
             {{ ('Забыли пароль') }}
         </a>
-    @endif
-    <x-primary-button class="ms-3">
-        {{ __('Вход') }}
-    </x-primary-button>
-</div>
+
     </form>
 </x-guest-layout>
