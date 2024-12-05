8a0fa2dcee869a96ef7a58ac13bd2f4579ae0c89
kseni <ksenialukianova2000@gmail.com>
2024-12-06 02:46:13 +0300

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -250,6 +250,30 @@
 </div>
 
 
+<div id="static-form-container" data-url="https://cp.unisender.com/ru/v5/subscribe-form/view/6tknymemmmwrmy3kze1r97sqn5ywfdx9n1zzk5fe" data-settings="6gpgck38nyzfb56ipwqom1hsuahxcct3zkrxz1o7uri9hppdtmaho" style="display: none;">
+    <form method="POST" action="https://cp.unisender.com/ru/subscribe?hash=6d4rrrmezwji386ipwqom1hsua6ruotoyyp7woaonige4a63pm15o" name="subscribtion_form" us_mode="embed">
+        <div class="container responsive">
+            <table cellpadding="0" cellspacing="0" align="center" style="width: 100%; box-sizing: border-box; float: left;">
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="subscribe_form">
+                                <h3>Рассылка</h3>
+                                <p>Ничего не пропустите! Подпишитесь, чтобы получать ежедневные предложения</p>
+                                <input type="text" name="email" class="form_input" placeholder="Почта..." required>
+                                <button type="submit" class="submit">Подписаться</button>
+                            </div>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        <input type="hidden" name="charset" value="UTF-8">
+        <input type="hidden" name="default_list_id" value="1">
+        <input type="hidden" name="overwrite" value="2">
+        <input type="hidden" name="is_v5" value="1">
+    </form>
+</div>
 
 </section>
 
