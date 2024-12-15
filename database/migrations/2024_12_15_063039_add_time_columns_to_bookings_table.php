02c709b0a653b1b9cec001faf0b3f47c96de05f3
kseni <ksenialukianova2000@gmail.com>
2024-12-15 09:31:06 +0300

    first commit

---
+++ database/migrations/2024_12_15_063039_add_time_columns_to_bookings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            $table->time('pickup_time')->nullable(); // Время начала бронирования
+            $table->time('return_time')->nullable(); // Время окончания бронирования
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('bookings', function (Blueprint $table) {
+            //
+        });
+    }
+};
