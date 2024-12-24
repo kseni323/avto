42eb8d51e7c30ad0266fe346b2ce5ec14b963d2d
kseni <ksenialukianova2000@gmail.com>
2024-12-24 05:52:01 +0300

    first commit

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -8,6 +8,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="css/app1.css">
+    <link rel="stylesheet" href="css/app232.css">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="stylesheet" href="css/phone.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
