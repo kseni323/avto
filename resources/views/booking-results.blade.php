c4f3ba42ff99a789f2949e132bb2dfd826da9542
kseni <ksenialukianova2000@gmail.com>
2024-12-25 07:29:57 +0300

    first commit

--- resources/views/booking-results.blade.php
+++ resources/views/booking-results.blade.php
@@ -6,8 +6,10 @@
     @foreach($bookings as $booking)
         <div class="card mb-3">
             <div class="card-body">
-                <h5 class="card-title">{{ $booking->car_model }}</h5>
-                <p class="card-text">Дата бронирования: {{ $booking->booking_date }}</p>
+                <p class="card-text">Место получения: {{ $booking->pickup_location	}}</p>
+                <p class="card-text">Место возврата: {{ $booking->return_location}}</p>
+                <p class="card-text">Дата получения: {{ $booking->pickup_date}}</p>
+                <p class="card-text">Дата возврата: {{ $booking->return_date}}</p>
                 <p class="card-text">Статус: {{ $booking->status }}</p>
                 <a href="{{ route('booking.edit', $booking->id) }}" class="btn btn-warning">Изменить</a>
                 <form action="{{ route('booking.cancel', $booking->id) }}" method="POST" class="d-inline">
