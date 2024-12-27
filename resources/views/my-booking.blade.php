9c0fcf2216be8329d1b41510a5cf0a0004037056
kseni <ksenialukianova2000@gmail.com>
2024-12-27 14:39:59 +0300

    first commit

--- resources/views/my-booking.blade.php
+++ resources/views/my-booking.blade.php
@@ -1,18 +1,228 @@
 @extends('header')
 
 @section('book_content')
-<div class="container">
-    <h1>Моё бронирование</h1>
+<style>
+    /* Обнуляем отступы и размеры */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+
+body, html {
+    height: 100%;
+    font-family: 'Montserrat', sans-serif;
+}
+
+.hero-section {
+    background-image: url('../images/sa.jpg'); /* Задаем фон */
+    background-position: 50% 50%;
+    background-size: cover;
+    background-repeat: no-repeat;
+    height: 500px;
+    padding: 150px 0px;
+}
+
+.content {
+    position: relative;
+    z-index: 1; 
+    text-align: left;
+    color: #fff; 
+    font-family: "Montserrat";
+    padding-left: 152px;
+}
+
+.content h1 {
+    font-size: 3em;
+    margin-bottom: 10px;
+}
+
+.content p {
+    font-size: 2em;
+    margin-bottom: 20px;
+}
+
+.btn {
+    display: inline-block;
+    padding: 10px 20px;
+    background: #04DBC0;
+    color: white;
+    text-decoration: none;
+    border-radius: 5px;
+    font-size: 1.1em;
+}
+
+.btn:hover {
+    background-color: #357fce;
+}
+
+.btn:not(:disabled):not(.disabled) {
+    cursor: pointer;
+}
+
+[type=button]:not(:disabled), [type=reset]:not(:disabled), [type=submit]:not(:disabled), button:not(:disabled) {
+    cursor: pointer;
+}
+
+@media (min-width: 992px) {
+    .navbar-expand-lg {
+        -ms-flex-flow: row nowrap;
+        flex-flow: row nowrap;
+        -ms-flex-pack: start;
+        justify-content: flex-start;
+    }
+}
+
+.navbar {
+    background-color: #000000;
+    position: relative;
+    display: -ms-flexbox;
+    display: flex;
+    -ms-flex-wrap: wrap;
+    flex-wrap: wrap;
+    -ms-flex-align: center;
+    align-items: center;
+    -ms-flex-pack: justify;
+    justify-content: space-between;
+    padding: .5rem 1rem;
+
+}
+
+.navbar-brand {
+    padding-top: var(--bs-navbar-brand-padding-y);
+    padding-bottom: var(--bs-navbar-brand-padding-y);
+    margin-right: var(--bs-navbar-brand-margin-end);
+    font-size: var(--bs-navbar-brand-font-size);
+    color: #fff;
+    text-decoration: none;
+    white-space: nowrap;
+    font-weight: bold;
+    font-family: "Montserrat";
+    font-size: 25px;
+}
+
+article, aside, figcaption, figure, footer, header, hgroup, main, nav, section {
+    display: block;
+}
+
+*, ::after, ::before {
+    box-sizing: border-box;
+}
+
+user agent stylesheet
+nav {
+    display: block;
+    unicode-bidi: isolate;
+}
+
+a {
+    background-color: transparent;
+}
+
+.nav-link {
+    display: block;
+    padding: var(--bs-nav-link-padding-y) var(--bs-nav-link-padding-x);
+    font-weight: var(--bs-nav-link-font-weight);
+    color: #fff;
+    text-decoration: none;
+    background: 0 0;
+    border: 0;
+    font-size: 17px;
+    font-family: "Montserrat";
+    font-weight: bold;
+}
+
+.section-title {
+    padding-top: 55px;
+    font-size: 40px;
+    line-height: 48px;
+    font-weight: 400;
+    letter-spacing: 0;
+    font-family: "Montserrat";
+    margin-bottom: 10px;
+    text-transform: uppercase;
+    text-align: center;
+    padding-bottom: 36px;
+}
+
+.col-lg-4{
+    position: relative;
+    padding-top: 25px;
+    font-weight: 200;
+    font-family: "Montserrat";
+    padding-bottom: 25px;
+    padding: 100px 0px;
+    color:#a2aab1;
+}
+
+
+.reservation_section .car-reservation-form {
+    box-shadow: 0 5px 40px rgb(0 0 0 / 10%);
+    padding: 10px 40px 40px;
+    background-color: white;
+}
+
+*, ::after, ::before {
+    box-sizing: border-box;
+}
+
+.form-control {
+    background-color: #fff;
+    border-radius: 0;
+    padding: 17px 10px;
+    box-shadow: none;
+    border: 2px solid #eee;
+    
+}
+
+.car-reservation-form label {
+    font-weight: bold;
+}
+label {
+    display: inline-block;
+    margin-bottom: .1rem;
+}
+
+
+user agent stylesheet
+form {
+    display: block;
+    margin-top: 0em;
+    unicode-bidi: isolate;
+    padding-top: 55px;
+}
+
+body {
+    font-family: "Work Sans", Helvetica, Arial, sans-serif;
+}
+
+body {
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", "Liberation Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    font-size: 1rem;
+    font-weight: 400;
+    color: #212529;
+    text-align: left;
+
+}
+
+element.style {
+    padding: 50px 0px;
+}
+
+</style>
+<div class="container mt-5">
+    <h1 class="mb-4">Моё бронирование</h1>
     @if(session('error'))
         <div class="alert alert-danger">{{ session('error') }}</div>
     @endif
-    <form action="{{ route('booking.find') }}" method="POST">
+    <form action="{{ route('booking.find') }}" method="POST" class="mt-3">
         @csrf
         <div class="form-group">
             <label for="email">Email</label>
-            <input type="email" name="email" id="email" class="form-control" placeholder="Введите Ваш Email" required>
+            <input type="email" name="email" id="email" class="form-control mb-3" placeholder="Введите Ваш Email" required>
         </div>
-        <button type="submit" class="btn btn-primary">Найти бронирование</button>
+        <button type="submit" class="btn btn-primary btn-block mt-3">Найти бронирование</button>
     </form>
 </div>
 @endsection
